23e94a1ce82883820c589db59bc082a3f46f1734
Vasyalvv <vasyalvv@gmail.com>
2021-03-13 20:37:49 +0600

    В контроллер Home  добавлен вид "Список сотрудников"

--- WebStore/Controllers/HomeController.cs
+++ WebStore/Controllers/HomeController.cs
@@ -3,11 +3,18 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebStore.Models;
 
 namespace WebStore.Controllers
 {
     public class HomeController : Controller
     {
+        private static readonly List<Employee> __Employees = new()
+        {
+            new Employee() { Id = 1, Age = 20, FirstName = "Иван", LastName = "Иванов", Patronymic = "Иванович" },
+            new Employee() { Id = 2, Age = 25, FirstName = "Петр", LastName = "Петров", Patronymic = "Петрович" },
+            new Employee() { Id = 3, Age = 30, FirstName = "Сидор", LastName = "Сидоров", Patronymic = "Сидорович" },
+        };
         public IActionResult Index()
         {
             return View();
@@ -16,6 +23,11 @@ namespace WebStore.Controllers
         public IActionResult SecondAction(string id)
         {
             return Content($"SecondAction\nid:{id}");
+        }
+
+        public IActionResult EmployeesList()
+        {
+            return View(__Employees);
         }
     }
 }
