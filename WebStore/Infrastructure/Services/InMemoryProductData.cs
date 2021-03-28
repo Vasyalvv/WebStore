f6acf115c463b0b7540a4d0ac1ebaa6244f745a6
Vasyalvv <vasyalvv@gmail.com>
2021-03-28 23:55:12 +0600

    Работа с товарами в сервисе

--- WebStore/Infrastructure/Services/InMemoryProductData.cs
+++ WebStore/Infrastructure/Services/InMemoryProductData.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using WebStore.Data;
+using WebStore.Domain;
 using WebStore.Domain.Entities;
 using WebStore.Infrastructure.Services.Interfaces;
 
@@ -11,6 +12,18 @@ namespace WebStore.Infrastructure.Services
     public class InMemoryProductData : IProductData
     {
         public IEnumerable<Brand> GetBrands() => TestData.Brands;
+
+        public IEnumerable<Product> GetProducts(ProductFilter Filter = null)
+        {
+            var query = TestData.Products;
+            if (Filter?.SectionId is { } section_id)
+                query = query.Where(product => product.SectionId == section_id);
+
+            if (Filter?.BrandId is { } brandId)
+                query = query.Where(product => product.BrandId == brandId);
+
+            return query;
+        }
 
         public IEnumerable<Section> GetSections() => TestData.Sections;
     }
--- WebStore/Infrastructure/Services/Interfaces/IProductData.cs
+++ WebStore/Infrastructure/Services/Interfaces/IProductData.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebStore.Domain;
 using WebStore.Domain.Entities;
 
 namespace WebStore.Infrastructure.Services.Interfaces
@@ -11,5 +12,7 @@ namespace WebStore.Infrastructure.Services.Interfaces
         IEnumerable<Section> GetSections();
 
         IEnumerable<Brand> GetBrands();
+
+        IEnumerable<Product> GetProducts(ProductFilter Filter = null);
     }
 }
