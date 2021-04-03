4e598d024c8f71a9c4eac2abef062080ab5ab38e
Vasyalvv <vasyalvv@gmail.com>
2021-04-03 16:03:07 +0600

    Подготовка сущностей для добавления их в БД

--- WebStore.Domain/Entities/Brand.cs
+++ WebStore.Domain/Entities/Brand.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,12 @@ using WebStore.Domain.Entities.Interfaces;
 
 namespace WebStore.Domain.Entities
 {
+    //[Table("Brands")] //Демонстрация атрибутов 
     public class Brand : NamedEntity, IOrderedEntity
     {
+        //[Column("BrandOrder",TypeName ="int")] //Демонстрация атрибутов 
         public int Order { get ; set ; }
+
+        public ICollection<Product> Products { get; set; }
     }
 }
