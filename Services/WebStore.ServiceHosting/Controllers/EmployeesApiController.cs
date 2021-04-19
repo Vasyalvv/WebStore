a1df48f75ec0a7ca69d68fbb4c3b5191c7488549
Vasyalvv <vasyalvv@gmail.com>
2021-04-19 21:53:36 +0600

    Контроллер WebAPI для управления сотрудниками

---
+++ Services/WebStore.ServiceHosting/Controllers/EmployeesApiController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebStore.Domain.Models;
+using WebStore.Interfaces.Services;
+
+namespace WebStore.ServiceHosting.Controllers
+{
+    [Route("api/employees")]
+    [ApiController]
+    public class EmployeesApiController : ControllerBase, IEmployeesData
+    {
+        private readonly IEmployeesData _EmployeesData;
+
+        public EmployeesApiController(IEmployeesData EmployeesData)
+        {
+            _EmployeesData = EmployeesData;
+        }
+
+        [HttpPost]
+        public int Add(Employee employee) => _EmployeesData.Add(employee);
+
+        [HttpPost("{employee}")] //post -> http://localhost:5001/api/employees/employee?LastName=Сергеев&FirstName=Александр&Patronymic=Петрович&Age=22
+        public Employee Add(string LastName, string FirstName, string Patronymic, int Age)
+            => _EmployeesData.Add(LastName, FirstName, Patronymic, Age);
+
+        [HttpDelete("{id}")]
+        public bool Delete(int id) => _EmployeesData.Delete(id);
+
+        [HttpGet]   //http://localhost:5001/api/employees
+        public IEnumerable<Employee> Get() => _EmployeesData.Get();
+
+        [HttpGet("{id}")]   //http://localhost:5001/api/employees/77
+        public Employee Get(int id) => _EmployeesData.Get(id);
+
+        [HttpGet("{employee}")] //http://localhost:5001/api/employees/employee?LastName=Сергеев&FirstName=Александр&Patronymic=Петрович
+        public Employee GetByName(string LastName, string FirstName, string Patronymic) 
+            => _EmployeesData.GetByName(LastName, FirstName, Patronymic);
+
+        [HttpPut]  //put -> http://localhost:5001/api/employees/77
+        public void Update(Employee employee) => _EmployeesData.Update(employee);
+    }
+}
