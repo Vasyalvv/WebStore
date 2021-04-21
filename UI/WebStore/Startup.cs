16369417f0eac07721bc12bed7cc7057e308a3f3
Vasyalvv <vasyalvv@gmail.com>
2021-04-21 22:09:53 +0600

    Регистрация клиента WebAPI каталога товаров в основном приложении в качестве сервиса

--- UI/WebStore/Startup.cs
+++ UI/WebStore/Startup.cs
@@ -11,6 +11,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using WebStore.Clients.Employees;
+using WebStore.Clients.Products;
 using WebStore.Clients.Values;
 using WebStore.DAL.Context;
 using WebStore.Domain.Entities.Identity;
@@ -80,7 +81,8 @@ namespace WebStore
             //services.AddTransient<IEmployeesData, InMemoryEmployeeData>();
             services.AddTransient<IEmployeesData, EmployeesClient>();
             //services.AddTransient<IProductData, InMemoryProductData>();
-            services.AddScoped<IProductData, SqlProductData>();
+            //services.AddScoped<IProductData, SqlProductData>();
+            services.AddScoped<IProductData, ProductsClient>();
             services.AddScoped<ICartServices, InCookiesCartService>();
             services.AddScoped<IOrderService, SqlOrderService>();
             services.AddScoped<IValuesService, ValuesClient>();
