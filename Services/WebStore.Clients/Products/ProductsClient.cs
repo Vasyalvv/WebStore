6163e0294968cedcd89a57ed1f8ee93cf8ee94fd
Vasyalvv <vasyalvv@gmail.com>
2021-04-21 22:04:47 +0600

    Клиент каталога товаров WebAPI

---
+++ Services/WebStore.Clients/Products/ProductsClient.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using WebStore.Clients.Base;
+using WebStore.Domain;
+using WebStore.Domain.DTO;
+using WebStore.Interfaces;
+using WebStore.Interfaces.Services;
+
+namespace WebStore.Clients.Products
+{
+    public class ProductsClient : BaseClient , IProductData
+    {
+        public ProductsClient(IConfiguration Configuration) : base(Configuration, WebAPI.Products)
+        {
+        }
+
+        public IEnumerable<BrandDTO> GetBrands() => base.Get<IEnumerable<BrandDTO>>($"{Address}/brands");
+        public ProductDTO GetProductById(int id) => Get<ProductDTO>($"{Address}/{id}");
+        public IEnumerable<ProductDTO> GetProducts(ProductFilter Filter = null) => 
+            Post(Address,Filter?? new ProductFilter())
+            .Content
+            .ReadAsAsync<IEnumerable<ProductDTO>>()
+            .Result;
+        public IEnumerable<SectionDTO> GetSections() => base.Get<IEnumerable<SectionDTO>>($"{Address}/sections");
+    }
+}
