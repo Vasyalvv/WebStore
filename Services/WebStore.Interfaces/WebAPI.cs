581eb9732a0d2672536a9c1ea77e59d3f80175f6
Vasyalvv <vasyalvv@gmail.com>
2021-04-24 18:11:20 +0600

    Заготовки контроллеров

--- Services/WebStore.Interfaces/WebAPI.cs
+++ Services/WebStore.Interfaces/WebAPI.cs
@@ -13,5 +13,10 @@ namespace WebStore.Interfaces
         public const string Products = "api/products";
         public const string Orders = "api/orders";
 
+        public static class Identity
+        {
+            public const string User = "api/users";
+            public const string Role = "api/roles";
+        }
     }
 }
---
+++ Services/WebStore.ServiceHosting/Controllers/Identity/UsersController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebStore.DAL.Context;
+using WebStore.Domain.Entities.Identity;
+using WebStore.Interfaces;
+
+namespace WebStore.ServiceHosting.Controllers.Identity
+{
+    [Route(WebAPI.Identity.User)]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly UserStore<User, Role, WebStoreDB> _UserStore;
+        public UsersController(WebStoreDB db)
+        {
+            _UserStore = new UserStore<User, Role, WebStoreDB>(db);
+        }
+    }
+}
