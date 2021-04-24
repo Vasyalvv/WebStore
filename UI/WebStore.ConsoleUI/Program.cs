0a2ebde47821e219e58958279d767b0616b6344c
Vasyalvv <vasyalvv@gmail.com>
2021-04-25 00:11:27 +0600

    Тестовый консольный клиент

---
+++ UI/WebStore.ConsoleUI/Program.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using WebStore.Clients.Products;
+
+namespace WebStore.ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var product_client = new ProductsClient(configuration);
+
+            Console.WriteLine("После запуска Hosting приложения нажмите любую клавишу");
+            Console.ReadKey();
+            foreach (var product in product_client.GetProducts())
+            {
+                Console.WriteLine($"{product.Id} - {product.Name} - {product.Price}");
+            }
+            Console.ReadLine();
+        }
+    }
+}
