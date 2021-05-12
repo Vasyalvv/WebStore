5b92fb23aafbed189eeff19fb29275a0f7b7d186
Vasyalvv <vasyalvv@gmail.com>
2021-05-12 21:26:50 +0600

    Хлебные крошки

---
+++ UI/WebStore/ViewModels/BreadCrumbsViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebStore.Domain.Entities;
+
+namespace WebStore.ViewModels
+{
+    public class BreadCrumbsViewModel
+    {
+        public Section Section { get; set; }
+
+        public Brand Brand { get; set; }
+
+        public string Product { get; set; }
+    }
+}
