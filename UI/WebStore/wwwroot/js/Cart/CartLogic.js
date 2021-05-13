0c4379f95fb5437e6194c648c687879b2518074d
Vasyalvv <vasyalvv@gmail.com>
2021-05-13 21:45:09 +0600

    Подготовка разметки для подключения скрипта

---
+++ UI/WebStore/wwwroot/js/Cart/CartLogic.js
@@ -0,0 +1,12 @@
+﻿Cart = {
+    _properties={
+        getCartViewLink: "",
+        addToCartLink: "",
+        decrementLink: "",
+        removeFromCart:""
+    },
+
+    init: function (propeties) {
+        $.extend(Cart._properties, propeties);
+    }
+}
