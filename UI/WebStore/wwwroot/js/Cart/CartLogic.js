98eeff03708d06e76816eb49b33d8f62bdd6a28a
Vasyalvv <vasyalvv@gmail.com>
2021-05-14 22:01:22 +0600

    Асинхронное увеличение товара в корзине

--- UI/WebStore/wwwroot/js/Cart/CartLogic.js
+++ UI/WebStore/wwwroot/js/Cart/CartLogic.js
@@ -8,9 +8,18 @@
 
     init: function (propeties) {
         $.extend(Cart._properties, propeties);
-
-        $(".add-to-cart").click(Cart.addToCart);
+        Cart.initEvents();
+        
     },
+
+    initEvents: function () {
+        $(".add-to-cart").click(Cart.addToCart);
+
+        $.(".cart_quntity_up").click(Cart.incrementItem);
+        $.(".cart_quntity_down").click();
+        $.(".cart_quntity_delete").click();
+    },
+
 
     addToCart: function (event) {
         event.preventDefault();
@@ -41,4 +50,48 @@
             })
             .fail(function () { console.log("refreshCartView fail"); });
     },
+
+    incrementItem: function (event) {
+        event.preventDefault();
+
+        var button = $(this);
+        const id = button.data("id");
+
+        var tr = button.closest("tr");
+
+        $.get(Cart._properties.addToCartLink + "/" + id)
+            .done(function () {
+                const count = parseInt($(".cart_quantity_input", tr).val());
+                $(".cart_quantity_input", tr).val(count + 1);
+                Cart.refreshPrice(tr);
+                Cart.refreshCartView();
+            })
+            .fail(function () { console.log("incrementItem fail"); });
+    },
+
+    refreshPrice: function (container) {
+        const count = parseInt($(".cart_quantity_input", tr).val());
+        const price = parseFloat($(".cart_price", container).data("price"));
+
+        const totalPrice = count * price;
+        const value = totalPrice.toLocaleString("ru-RU", { style: "currency", currency: "RUB" });
+
+        const cartTotalPrice = $(".cart_total_price", container);
+        cartTotalPrice.data("price", totalPrice);
+        cartTotalPrice.html(value);
+
+        Cart.refreshTotalPrice();
+    },
+
+    refreshTotalPrice: function () {
+        var totalPrice = 0;
+
+        $(".cart_total_price").each(function () {
+            const price = parseFloat($(this).data("price"));
+            totalPrice += price;
+        });
+
+        const value = totalPrice.toLocaleString("ru-RU", { style: "currency", currency: "RUB" });
+        $("#total-order-price").html(value);
+    }
 }
