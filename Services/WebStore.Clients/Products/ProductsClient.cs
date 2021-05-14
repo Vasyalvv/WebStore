b1b64fb041151ebf6060e88b440c1d09ac5d6938
Vasyalvv <vasyalvv@gmail.com>
2021-05-14 23:11:39 +0600

    Внедрение постраничного разбиения каталога товаров

--- Services/WebStore.Clients/Products/ProductsClient.cs
+++ Services/WebStore.Clients/Products/ProductsClient.cs
@@ -22,10 +22,10 @@ namespace WebStore.Clients.Products
         public BrandDTO GetBrandById(int id) => Get<BrandDTO>($"{Address}/brands/{id}");
         public IEnumerable<BrandDTO> GetBrands() => base.Get<IEnumerable<BrandDTO>>($"{Address}/brands");
         public ProductDTO GetProductById(int id) => Get<ProductDTO>($"{Address}/{id}");
-        public IEnumerable<ProductDTO> GetProducts(ProductFilter Filter = null) => 
+        public PageProductsDTO GetProducts(ProductFilter Filter = null) => 
             Post(Address,Filter?? new ProductFilter())
             .Content
-            .ReadAsAsync<IEnumerable<ProductDTO>>()
+            .ReadAsAsync<PageProductsDTO>()
             .Result;
         public SectionDTO GetSectionById(int id) => Get<SectionDTO>($"{Address}/sections/{id}");
         public IEnumerable<SectionDTO> GetSections() => base.Get<IEnumerable<SectionDTO>>($"{Address}/sections");
--- UI/WebStore/Controllers/CatalogController.cs
+++ UI/WebStore/Controllers/CatalogController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,22 +14,32 @@ namespace WebStore.Controllers
     public class CatalogController : Controller
     {
         private readonly IProductData _ProductData;
+        private readonly IConfiguration _Configuration;
 
-        public CatalogController(IProductData ProductData)
+        public CatalogController(IProductData ProductData, IConfiguration Configuration)
         {
             _ProductData = ProductData;
+            _Configuration = Configuration;
         }
 
-        public IActionResult Index(int? BrandId, int? SectionId)
+        public IActionResult Index(int? BrandId, int? SectionId, int Page = 1, int? PageSize = null)
         {
-            var filter = new ProductFilter { BrandId = BrandId, SectionId = SectionId };
+            var page_size = PageSize
+                ?? (int.TryParse(_Configuration["CatalogPageSize"], out var value) ? value : null);
+
+            var filter = new ProductFilter { 
+                BrandId = BrandId,
+                SectionId = SectionId ,
+                Page=Page,
+                PageSize=page_size
+            };
             var products = _ProductData.GetProducts(filter);
 
             return View(new CatalogViewModel
             {
                 SectionId = SectionId,
                 BrandId = BrandId,
-                Products = products
+                Products = products.Products
             .OrderBy(p => p.Order).FromDTO()
             //.Select(p => new ProductViewModel
             //{
@@ -38,8 +49,7 @@ namespace WebStore.Controllers
             //    Price = p.Price
             //})
             .ToView()
-            }
-            );
+            });
         }
 
         public IActionResult Details(int id)
